648d8c221b44d5de3ff87e15f516a28bdb9c671f
GammaGammaRay <benjamin.peret@gmail.com>
2023-06-01 03:15:49 -0300

    login and register screens layout, routes, navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Dependencies
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./style/GlobalStyle.js";
+import ResetStyle from "./style/ResetStyle.js";
+import styled from "styled-components";
+import axios from "axios";
+// Pages
+import Login from "./screens/Login.jsx";
+import RegisterUser from "./screens/RegisterUser.jsx";
+import Habitos from "./screens/Habitos.jsx";
+import Historico from "./screens/Historico.jsx";
+import Hoje from "./screens/Hoje.jsx";
+// Components
+import Navbar from "./components/Navbar.jsx";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <GlobalStyle />
+      <ResetStyle />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<RegisterUser />} />
+        <Route
+          path="/habitos"
+          element={
+            <>
+              <Navbar />
+              <Habitos />
+            </>
+          }
+        />
+        <Route
+          path="/hoje"
+          element={
+            <>
+              <Navbar />
+              <Hoje />
+            </>
+          }
+        />
+        <Route
+          path="/historico"
+          element={
+            <>
+              <Navbar />
+              <Historico />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

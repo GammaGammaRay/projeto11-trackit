db0c93fd1878dd54905fa93ac73dae41f193d23c
GammaGammaRay <benjamin.peret@gmail.com>
2023-06-05 00:36:11 -0300

    full code refactor, habit wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,37 @@
 // Dependencies
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GlobalStyle from "./style/GlobalStyle.js";
-import ResetStyle from "./style/ResetStyle.js";
-import styled from "styled-components";
-import axios from "axios";
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+//Context
+import PageContextProvider from "./components/ContextProvider.jsx";
 
 // Pages
-import Login from "./screens/Login.jsx";
-import RegisterUser from "./screens/RegisterUser.jsx";
-import Habitos from "./screens/Habitos.jsx";
-import Historico from "./screens/Historico.jsx";
-import Hoje from "./screens/Hoje.jsx";
+import Login from "./pages/LoginPage.jsx";
+import RegisterUser from "./pages/RegisterPage.jsx";
+import Habitos from "./pages/HabitosPage.jsx";
+import Historico from "./pages/HistoricoPage.jsx";
+import Hoje from "./pages/HojePage.jsx";
 
 // Components
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
-import { UserContext } from "./components/Context.js";
 
-axios.defaults.headers.common["Authorization"] = "5wY65Pq5uCOFfh5yfnMVGhIF";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <ResetStyle />
+    <PageContextProvider>
+      {pathname !== "/" && pathname !== "/cadastro" && <Navbar />}
       <Routes>
         <Route path="/" element={<Login />} />
-
         <Route path="/cadastro" element={<RegisterUser />} />
-        <Route
-          path="/habitos"
-          element={
-            <>
-              <Navbar />
-              <Footer />
-              <Habitos />
-            </>
-          }
-        />
-        <Route
-          path="/hoje"
-          element={
-            <>
-              <Navbar />
-              <Footer />
-              <Hoje />
-            </>
-          }
-        />
-        <Route
-          path="/historico"
-          element={
-            <>
-              <Navbar />
-              <Footer />
-              <Historico />
-            </>
-          }
-        />
+        <Route path="/habitos" element={<Habitos />} />
+        <Route path="/hoje" element={<Hoje />} />
+        <Route path="/historico" element={<Historico />} />
       </Routes>
-    </BrowserRouter>
+      {pathname !== "/" && pathname !== "/cadastro" && <Footer />}
+    </PageContextProvider>
   );
 }
 
